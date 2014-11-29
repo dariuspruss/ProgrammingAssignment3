b6f26ce79e9194d58e533f3bff188df7928708fa
dariuspruss <darius.pruss@gmail.com>
2014-11-28 17:07:11 -0800

    solved best parts 1-3

--- best.R
+++ best.R
@@ -1,31 +1,26 @@
 best <- function(state, outcome) {
 
-dat <-  read.csv("outcome-of-care-measures.csv", na.strings = "Not Available")
+dat <-  read.csv("outcome-of-care-measures.csv", colClasses  =  "character", na.strings = "Not Available")
 
-## Check that state and outcome are valid
+if(state %in% dat$State) {
 
-## Return hospital name in that state with lowest 30-day death rate
-
-if (outcome == "heart attack") {
-	subs <- (subset(dat, State == state))
-	subs <- subs[ order(subs[,11]), ]
-	subs[[1,2]]	
-} 
-else if (outcome == "heart failure") {
+	if (outcome == "heart attack") {
 		subs <- (subset(dat, State == state))
-		subs <- subs[ order(subs[,17]), ]
-		subs[[1,2]]
-} 
-else if (outcome == "pneumonia") {
+		subs <- subs[ order(as.numeric(subs[,11])), ]
+		subs[[1,2]]	
+	} 
+	else if (outcome == "heart failure") {
 		subs <- (subset(dat, State == state))
-		subs <- subs[ order(subs[,23]), ]
+		subs <- subs[ order(as.numeric(subs[,17])), ]
 		subs[[1,2]]
-} 
-else {
-	print("invalid outcome")
+	} 
+	else if (outcome == "pneumonia") {
+		subs <- (subset(dat, State == state))
+		subs <- subs[ order(as.numeric(subs[,23])), ]
+		subs[[1,2]]
+	} 
+	else stop("invalid outcome")   
+
+} else stop("invalid state")   
 }
-
-
-
-
 
